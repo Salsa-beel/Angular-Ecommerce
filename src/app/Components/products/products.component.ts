8acfcc530048033a5e53d0a57cb75ed8b8c943d3
Salsabeel <salsabeelhatem52@yahoo.com>
2023-03-31 03:25:31 +0200

    added filter and created product details service

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/service/api';
+import { ProductsService } from 'src/app/service/products.service';
 import { CartService } from 'src/app/service/cart.service';
 
 @Component({
@@ -9,21 +9,44 @@ import { CartService } from 'src/app/service/cart.service';
 })
 export class ProductsComponent implements OnInit {
 
-  public productList : any ;
+  products : any [] = [];
+catgories :any[]=[];
 grandTotal: any;
-  constructor( private api :ApiService, private cartService :CartService ) { }
+categories: any;
+  constructor( private service :ProductsService, private cartService :CartService ) { }
 
   ngOnInit():void {
-    this.api.getProduct().subscribe(res=>{
-      this.productList = res;
-      this.productList.forEach((a: any) =>{
-        Object.assign(a,{quantity:1,total:a.price})
-      });
-    })
+    this.getAllProducts()
+    this.getAllCategories()
   }
 
 
   addtoCart(item: any){
 this.cartService.addtoCart(item);
+  }
+
+  getAllProducts(){
+    this.service.getAllProducts().subscribe((res:any)=>{
+
+    this.products=res // kol da fe 7alet el success
+  }, error =>{
+    alert("Error") // da fe 7alet el error
+  }     )
+  }
+
+
+  getAllCategories(){
+    this.service.getAllCategories().subscribe((res:any)=>{
+      console.log(res)
+    this.categories=res // kol da fe 7alet el success
+  }, error =>{
+    alert("Error") // da fe 7alet el error
+  }     )
+  }
+
+  filterCategory(event : any) {
+
+    let value = event.target.value;
+    console.log(value)
   }
 }
--- src/app/service/api.ts
+++ src/app/service/api.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators'
+// import { Injectable } from '@angular/core';
+// import {HttpClient } from '@angular/common/http';
+// import {map} from 'rxjs/operators'
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ApiService {
 
-constructor( private http : HttpClient) { }
+// constructor( private http : HttpClient) { }
 
-getProduct(){
-  return this.http.get<any> ("https://fakestoreapi.com/products/")
-  .pipe(map((res:any)=>{
-return res;
-  }))
+// getProduct(){
+//   return this.http.get<any> ("https://fakestoreapi.com/products/")
+//   .pipe(map((res:any)=>{
+// return res;
+//   }))
 
-}
+// }
 
-}
+// }
